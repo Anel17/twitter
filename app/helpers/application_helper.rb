ffeb1769d203eb0018fbd6d67f2e066b13a92533
anel <manel_1999@mail.ru>
2014-12-02 12:52:54 +0000

    add registration

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+    
+    def full_title(title)
+        if title.empty?
+            return "Twitter"
+        else
+            return title + " | Twitter"
+        end    
+    
+    end
+    
 end
